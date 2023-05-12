4c029f97da20e3e9d35d25003668323f29c03c57
MarB-T <danmarbet@gmail.com>
2023-05-12 05:45:49 +0300

    Task8: correction

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-def magic_calculation_102(a, b):
-    c = 0
+from magic_calculation_102 import add, sub
+
+def magic_function(a, b):
     if a < b:
         c = add(a, b)
-        for i in range(4, 7):
+        for i in range(4, 6):
             c = add(c, i)
+        return c
     else:
-        c = add(c, i)
-    return c
+        return sub(a, b)
+
