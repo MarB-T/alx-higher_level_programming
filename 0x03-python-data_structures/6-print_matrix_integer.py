c474c4e4386c62c1b0d3cc558af5c1f3c0a85978
MarB-T <danmarbet@gmail.com>
2023-05-15 16:56:44 +0300

    Project tasks furter updates

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 0 or len(matrix[0]) == 0:
+    if len(matrix) == 1 and len(matrix[0]) == 0:
         print("")
         return None
     for lis in matrix:
