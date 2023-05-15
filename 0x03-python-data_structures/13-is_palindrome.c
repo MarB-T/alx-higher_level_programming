4b68ef5e1b1245538787bc94b52e6b393c9dc81f
MarB-T <danmarbet@gmail.com>
2023-05-16 02:01:20 +0300

    Palindrome task update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,48 +1,85 @@
 #include "lists.h"
 
 /**
- * is_palindrome - function to check if linked list is palindrome
- * @head: pointer to linked list head
- * Return: 1 if palindrome 0 if not palindrome
+ * reverse_linked_list - reverses a linked list
+ * @head: pointer to linked list
+ * Return: revesed linked list
+ */
+
+listint_t *reversed_linked_list(listint_t **head)
+{
+	listint_t *current = *head, *next, *prev = NULL;
+    
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+    
+	return (prev);
+}
+
+/**
+ * is_palindrome - checks if linked list is a palindrome
+ * @head: pointer to the list in question
+ * Return: 1-is palindrome 0-is not palindrome
  */
 
 int is_palindrome(listint_t **head)
 {
-	int count = 0, left = 0, right, *arr;
-	int i = 0;
-	listint_t *current;
-	current = *head;
-	while (current != NULL)
-	{
-		count++;
-		current = current->next;
-	}
+	listint_t *slow = *head;
+	listint_t *fast = *head, *p1, *p2;
+	listint_t *prev_slow = *head;
+	listint_t *mid_node = NULL, *second_half;
+	int is_palindrome = 1;
 
-	arr = malloc(count * sizeof(int));
-	if (arr == NULL)
+	if (*head != NULL && (*head)->next != NULL)
 	{
-		return (0);
-	}
-
-	current = *head;
-	while (current != NULL)
-	{
-		arr[i++] = current->n;
-		current = current->next;
-	}
-
-	right = count - 1;
-	while (left < right)
-	{
-		if (arr[left] != arr[right])
+		while (fast != NULL && fast->next != NULL)
 		{
-			free(arr);
-			return (0);
+			fast = fast->next->next;
+			prev_slow = slow;
+			slow = slow->next;
 		}
-		left++;
-		right--;
+
+		if (fast != NULL)
+		{
+			mid_node = slow;
+			slow = slow->next;
+		}
+
+		second_half = slow;
+		prev_slow->next = NULL;
+		second_half = reversed_linked_list(&second_half);
+
+		p1 = *head;
+		p2 = second_half;
+
+		while (p1 != NULL && p2 != NULL)
+		{
+			if (p1->n != p2->n)
+			{
+				is_palindrome = 0;
+				break;
+			}
+		p1 = p1->next;
+		p2 = p2->next;
+		}
+
+		second_half = reversed_linked_list(&second_half);
+
+		if (mid_node != NULL)
+		{
+			prev_slow->next = mid_node;
+			mid_node->next = second_half;
+		}
+		else
+		{
+			prev_slow->next = second_half;
+		}
 	}
 
-	free(arr);
-	return (1);
+	return (is_palindrome);
 }
---
+++ 0x03-python-data_structures/palindrome.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+
+
+/**
+ * is_palindrome - function to check if linked list is palindrome
+ * @head: pointer to linked list head
+ * Return: 1 if palindrome 0 if not palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int count = 0, left = 0, right, *arr;
+	int i = 0;
+	listint_t *current;
+	current = *head;
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+
+	arr = malloc(count * sizeof(int));
+	if (arr == NULL)
+	{
+		return (0);
+	}
+
+	current = *head;
+	while (current != NULL)
+	{
+		arr[i++] = current->n;
+		current = current->next;
+	}
+
+	right = count - 1;
+	while (left < right)
+	{
+		if (arr[left] != arr[right])
+		{
+			free(arr);
+			return (0);
+		}
+		left++;
+		right--;
+	}
+
+	free(arr);
+	return (1);
+}
