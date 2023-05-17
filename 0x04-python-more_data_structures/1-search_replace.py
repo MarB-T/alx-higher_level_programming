a53f71079c80e5c338667342ccf4eefa3ea34be9
MarB-T <danmarbet@gmail.com>
2023-05-17 15:33:00 +0300

    Task1: search and replace in list correction

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,10 +2,9 @@
 def search_replace(mylist, search, replace):
     l = len(mylist)
     new_list = []
-    for i in range(l):
+    for i in range(l - 1):
         if i == search:
             new_list.append(replace)
         else:
             new_list.append(mylist[i])
-
     return (new_list)
