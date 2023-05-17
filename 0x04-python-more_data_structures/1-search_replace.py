00fce014c91797329802df995b1ce7616ecf2a54
MarB-T <danmarbet@gmail.com>
2023-05-17 15:28:49 +0300

    Task1: search and replace in list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def search_replace(mylist, search, replace):
+    l = len(mylist)
+    new_list = []
+    for i in range(l):
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(mylist[i])
+
+    return (new_list)
