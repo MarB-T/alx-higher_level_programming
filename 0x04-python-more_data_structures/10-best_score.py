56520eb80ec4dc2250c1188186d6a9c462ae67cb
MarB-T <danmarbet@gmail.com>
2023-05-17 22:54:39 +0300

    Task10: returns the biggest value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return (None)
+    best = 0
+    for key, value in a_dictionary.items():
+        if value > best:
+            best_key = key
+            best = value
+    return (best_key)
