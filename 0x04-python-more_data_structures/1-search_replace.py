e622cf806dabf122905a3a349671943a9965fb1e
MarB-T <danmarbet@gmail.com>
2023-05-17 16:16:32 +0300

    Task1: search and replace in list correction

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def search_replace(mylist, search, replace):
-    l = len(mylist)
+    length = len(mylist)
     new_list = []
-    for i in range(l):
+    for i in range(length):
         if mylist[i] == search:
             new_list.append(replace)
         else:
