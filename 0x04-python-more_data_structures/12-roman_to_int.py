7290d2c819ecc3ccbf6fd1644c20ddb56ee6899c
MarB-T <danmarbet@gmail.com>
2023-05-18 16:48:24 +0300

    project 0x04 tasks correction

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if roman_string is None:
         return (0)
-    elif isinstance(roman_string, str) == False:
+    elif isinstance(roman_string, str) is False:
         return (0)
     roman_map = {
         'I': 1,
@@ -15,15 +15,11 @@ def roman_to_int(roman_string):
     }
     result = 0
     prev_value = 0
-    
     for c in reversed(roman_string):
         current_value = roman_map[c]
-        
         if current_value >= prev_value:
             result += current_value
         else:
             result -= current_value
-        
         prev_value = current_value
-    
     return result
