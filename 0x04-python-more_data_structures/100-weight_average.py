7377d5e152a2076623d655eed3b1061746021b3c
MarB-T <danmarbet@gmail.com>
2023-05-18 16:40:50 +0300

    project 0x04 tasks

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     num = 0
     den = 0
+    if my_list is None:
+        return (0)
     for item in my_list:
         num += (item[0] * item[1])
         den += item[1]
