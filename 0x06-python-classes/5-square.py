f9dee0853d9ceb62c556291de31d27f83d5e0215
MarB-T <danmarbet@gmail.com>
2023-05-31 00:34:56 +0300

    Task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+    class Square declaration
+
+
+
+"""
+
+
+class Square:
+    """ objects and variables of class """
+    def __init__(self, size=0):
+        """ Object initialization
+        Args:
+            size
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Gets size of square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of a square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """ Prints # square """
+        if Square.size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
