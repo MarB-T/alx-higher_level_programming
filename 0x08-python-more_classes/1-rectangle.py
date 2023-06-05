b79e6ab0c39057b297f955f496b5bb44b3c28942
MarB-T <danmarbet@gmail.com>
2023-06-06 02:29:03 +0300

    add height and width to rectangle correction

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,26 +10,30 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    @property
     def width(self):
         """ reutrns the width of rectangle """
-        return (self.width)
+        return (self.__width)
 
+    @width.setter
     def width(self, value=0):
         """ sets the width of the rectangle """
         if value < 0:
             raise ValueError("width must be >= 0")
         if isinstance(value, int) is False:
             raise TypeError("width must be an integer")
-        self.width = value
+        self.__width = value
 
+    @property
     def height(self):
         """ returns the height of the rectangle """
         return (self.height)
 
+    @height.setter
     def height(self, value=0):
         """ sets the value for height """
         if value < 0:
             raise ValueError("width must be >= 0")
-        if isinstance(vale, int) is False:
+        if isinstance(value, int) is False:
             raise TypeError("width must be an integer")
-        self.height = value
+        self.__height = value
