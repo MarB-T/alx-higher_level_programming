c8668d31e30c87e27f17fae42b91ccf8e494b684
MarB-T <danmarbet@gmail.com>
2023-06-06 03:08:59 +0300

    add height and width to rectangle correction

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,15 +2,6 @@
 """ The rectangle Module """
 
 
-def check_value(value):
-    """ function to check value """
-    if value < 0:
-        raise ValueError("width must be >= 0")
-    if isinstance(value, int) is False:
-        raise TypeError("width must be an integer")
-    return (True)
-
-
 class Rectangle:
     """ Definition of class Rectangle """
 
@@ -26,27 +17,23 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """ sets the width of the rectangle
-        if value < 0:
-            raise ValueError("width must be >= 0")
+        """ sets the width of the rectangle """
         if isinstance(value, int) is False:
             raise TypeError("width must be an integer")
-        """
-        if check_value(value) is True:
-            self.__width = value
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
         """ returns the height of the rectangle """
-        return (self.height)
+        return (self.__height)
 
     @height.setter
     def height(self, value):
-        """ sets the value for height
-        if value < 0:
-            raise ValueError("width must be >= 0")
+        """ sets the value for height """
         if isinstance(value, int) is False:
-            raise TypeError("width must be an integer")
-        """
-        if check_value(value) is True:
-            self.__height = value
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
