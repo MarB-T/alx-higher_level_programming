3f74b495fb3dd4e63ba985e09b8a6171e4db5680
MarB-T <danmarbet@gmail.com>
2023-06-12 22:54:38 +0300

    function to check is an object belongs to a class - inheritance not onsidered pycodestyle correction

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module definition goes here... """
 
+
 def is_same_class(obj, a_class):
     """ function to  check if an object belongs to a class """
     if type(obj) == a_class:
