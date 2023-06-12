3e2dab672150fffd4109f40ba65ce1716ea31e31
MarB-T <danmarbet@gmail.com>
2023-06-12 23:28:36 +0300

    empty method area in class BaseGeometr

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" module docstring goes here... """
+
+
+class BaseGeometry:
+    """ class BaseGeometry definition """
+    def area(self):
+        """ calculates area """
+        raise Exception("area() is not implemented")
