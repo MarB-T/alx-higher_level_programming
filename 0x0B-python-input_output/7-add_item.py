b7eb4cd3c36222c3592d864730f1de1102a2b2d8
MarB-T <danmarbet@gmail.com>
2023-06-14 01:19:21 +0300

    script to add all arguments to a python list and save to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ module docstring """
 
-
-import json, sys
+import sys
+import json
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
