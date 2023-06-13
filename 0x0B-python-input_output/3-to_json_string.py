d755b793a30591c12b4d87a169df08ef83be64d9
MarB-T <danmarbet@gmail.com>
2023-06-13 23:50:45 +0300

    function to retuurn the json representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" module docstring goes here """
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ returns a JSON representation of an object """
+    return (json.dumps(my_obj))
