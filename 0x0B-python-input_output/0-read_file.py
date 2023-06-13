689c40fd8215d0eac05510f51c364a0236e52fba
MarB-T <danmarbet@gmail.com>
2023-06-13 21:10:55 +0300

    Task0 correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """ function to read a text file (UTF8) and print to stdout """
     with open(filename, encoding='UTF8') as f:
-            print(f.read(), end='')
+        print(f.read(), end='')
