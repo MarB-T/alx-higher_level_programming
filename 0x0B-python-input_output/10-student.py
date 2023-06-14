cb03250459a47575fcf17f03121cb441fd54191c
MarB-T <danmarbet@gmail.com>
2023-06-14 05:39:00 +0300

    Tasks on class Student correction

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -2,10 +2,18 @@
 """ module docstring """
 
 
-Student = __import__('9-student').Student
-
-class Student(Student):
+class Student:
     """ student class based of similar class """
+    def __init__(self, first_name, last_name, age):
+        """ initalizer """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns dictionary representation of student class"""
+        return (self.__dict__)
+
     def to_json(self, attrs=None):
         """returns dictionary representation of instance"""
         if attrs is None:
--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -2,11 +2,29 @@
 """ module docstring """
 
 
-Student = __import__('10-student').Student
-
-
-class Student(Student):
+class Student:
     """ classs student based of 10-student.py """
+    def __init__(self, first_name, last_name, age):
+        """ initalizer """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns dictionary representation of student class"""
+        return (self.__dict__)
+
+    def to_json(self, attrs=None):
+        """returns dictionary representation of instance"""
+        if attrs is None:
+            return (self.__dict__)
+        if not isinstance(attrs, list):
+            return (self.__dict__)
+        if not (isinstance(attrs[elem], str) for elem in attrs):
+            return (self.__dict__)
+        return ({key: value for key, value in self.__dict__.items()
+                 if key in attrs})
+
     def reload_from_json(self, json):
         for key, value in json.items():
             setattr(self, key, value)
