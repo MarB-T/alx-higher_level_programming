824635ca832753ac0cf2c23303e0d30710f90e3d
MarB-T <danmarbet@gmail.com>
2023-06-19 23:48:25 +0300

    update the update method to take wargs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,26 +95,44 @@ class Rectangle(Base):
         """ returns the rectangle string """
         return (
             f"[Rectangle] ({self.id}) {self.__x}"
-            f" /{self.__y} - {self.width}/{self.__height}"
+            f"/{self.__y} - {self.width}/{self.__height}"
             )
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """ updates rectangle attributes """
-        keys = ["id", "width", "height", "x", "y"]
-        attributes = dict(zip(keys, args))
+        if len(args) != 0:
+            keys = ["id", "width", "height", "x", "y"]
+            attributes = dict(zip(keys, args))
 
-        for key, value in attributes.items():
-            if isinstance(value, int) is False:
-                raise TypeError(f"{value} must be an integer")
-            if value < 0:
-                raise ValueError(f"{value} must be >= 0")
-        if args[0]:
-            super().__init__(args[0])
-        if len(args) >= 2:
-            self.__width = args[1]
-        if len(args) >= 3:
-            self.__height = args[2]
-        if len(args) >= 4:
-            self.__x = args[3]
-        if len(args) >= 5:
-            self.__y = args[4]
+            for key, value in attributes.items():
+                if isinstance(value, int) is False:
+                    raise TypeError(f"{value} must be an integer")
+                if value < 0:
+                    raise ValueError(f"{value} must be >= 0")
+            if args[0]:
+                super().__init__(args[0])
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
+
+        else:
+            for key, value in kwargs.items():
+                if isinstance(value, int) is False:
+                    raise TypeError(f"{value} must be an integer")
+                if value < 0:
+                    raise ValueError(f"{value} must be >= 0")
+                if 'id' in kwargs:
+                    super().__init__(kwargs['id'])
+                if 'width' in kwargs:
+                    self.__width = kwargs['width']
+                if 'height' in kwargs:
+                    self.__height = kwargs['height']
+                if 'x' in kwargs:
+                    self.__x = kwargs['x']
+                if 'y' in kwargs:
+                    self.__y = kwargs['y']
