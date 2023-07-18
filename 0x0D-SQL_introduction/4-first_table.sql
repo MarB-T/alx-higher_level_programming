49ce5aec6eb17c65b6a9954e6dd391aefd38e3f7
MarB-T <danmarbet@gmail.com>
2023-07-18 22:50:48 +0300

    Task4: create table if not exist correction

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,8 @@
 -- Script that creates a table called first_table in the current database
-CREATE TABLE IF NOT EXISTS first_table ('id' INT, 'name' VARCHAR(256));
+-- The database name will be passed as an argument of the mysql command
+-- first_table description:
+--             id INT
+--             name VARCHAR(256)
+-- If table first_table already exists, your script should not fail
+
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
