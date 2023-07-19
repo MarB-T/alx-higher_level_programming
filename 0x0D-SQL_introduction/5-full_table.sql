774ad5e6c67575228177c6b792ec883fc138d68e
MarB-T <danmarbet@gmail.com>
2023-07-19 05:32:37 +0300

    Scripts to show table details

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- Script to print the full dscription of the table first_table from database hbtn_0_0
+SHOW CREATE table first_table;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- Script to list all rowa of table first_table
+SELECT * FROM first_table;
