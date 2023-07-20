1fc8e8c92c3e447e0b4b906bcce7d635968366a6
MarB-T <danmarbet@gmail.com>
2023-07-20 04:47:22 +0300

    update 0-privileges file

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Script to list privileges of users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW IF EXISTS GRANTS FOR 'user_0d_1'@'localhost';
+SHOW IF EXISTS GRANTS FOR 'user_0d_2'@'localhost';
