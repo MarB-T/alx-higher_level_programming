b2e35ed9c5e96617ce0a490dfede143d6dd872a4
MarB-T <danmarbet@gmail.com>
2023-08-15 05:34:28 +0300

    prints string multiple times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let times = process.argv[2];
+if (!parseInt(times)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i =0;
+  while (i < times) {
+    console.log('C is fun');
+    i++;
+  }
+}
