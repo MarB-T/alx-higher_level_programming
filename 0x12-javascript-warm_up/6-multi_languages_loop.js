0d85b6dd787774cfccb39ed597d3819ad591b7f5
MarB-T <danmarbet@gmail.com>
2023-08-15 05:20:55 +0300

    Using a loop (for) to print

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i in lang; i++) {
-  console.log(lang[i]);
+const toPrint = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(toPrint[i]);
 }
