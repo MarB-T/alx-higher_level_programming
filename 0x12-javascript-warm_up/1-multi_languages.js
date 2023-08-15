2a52220e19277bda2b30611550667d9ad9631c40
MarB-T <danmarbet@gmail.com>
2023-08-15 04:38:39 +0300

    Script to print three lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-let firstLine = 'C is fun';
-let secondLine = 'Python is cool';
-let thirdLine = 'JavaScript is amazing';
-console.log(firstLine);
-console.log(secondLine);
-console.log(thirdLine);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
