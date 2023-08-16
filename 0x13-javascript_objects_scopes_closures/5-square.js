d99b0932c05ae659b213f560427cbaa1db1edb15
MarB-T <danmarbet@gmail.com>
2023-08-16 04:34:56 +0300

    class inheritance correction

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,8 +3,10 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    super(size, size);
-    this.size = size;
+    if (size > 0) {
+      super(size, size);
+      this.size = size;
+    }
   }
 }
 module.exports = Square;
