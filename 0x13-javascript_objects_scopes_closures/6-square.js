cfe6d0412ab564b378e6057bcde40ee73c9bd62b
MarB-T <danmarbet@gmail.com>
2023-08-16 04:56:01 +0300

    Using super class method in a sub class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const Rectangle = require('./5-square');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let y = 0; y < this.height; y++) {
+        for (let x = 0; x < this.width; x++) {
+          process.stdout.write('C');
+        }
+        console.log();
+      }
+    } else {
+      super.print();
+    }
+  }
+}
+module.exports = Square;
