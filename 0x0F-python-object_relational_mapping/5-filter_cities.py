9a5c40779c744549b84f52322ad554d9488fa246
MarB-T <danmarbet@gmail.com>
2023-08-20 20:38:02 +0300

    script to list all cities in a state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+script to get all the cities in a given state
+"""
+import MySQLdb
+from sys import argv
+
+
+def all_cities_in_state():
+    """
+    function to get all the cities in a given state
+    """
+    conn = MySQLdb.connect(host='localhost',
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3])
+    cur = conn.cursor()
+    query = ("SELECT cities.name "
+             "FROM cities "
+             "JOIN states ON cities.state_id = states.id "
+             "WHERE states.name = %(name)s "
+             "ORDER BY cities.id")
+    search_state = {'name': argv[4]}
+    cur.execute(query, search_state)
+    cities = cur.fetchall()
+    for city in cities:
+        print(city)
+
+    cur.close()
+
+
+if __name__ == '__main__':
+    all_cities_in_state()
