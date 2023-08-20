f4933bca77e117c3f7afb0746c386f0a9bfd9bdd
MarB-T <danmarbet@gmail.com>
2023-08-20 20:01:29 +0300

    script to fetch from two  tables using JOIN statement

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+script that fetches from two tables using JOIN
+"""
+import MySQLdb
+from sys import argv
+
+
+def cities_by_state():
+    """ Function to get cities by states """
+    conn = MySQLdb.connect(host='localhost',
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3])
+    cur = conn.cursor()
+    query = ("SELECT cities.name, states.name"
+             "FROM cities"
+             "JOIN states ON cities.states_id = states.id"
+             "ORDER BY cities.id")
+    cur.execute(query)
+    cities = cur.fetchall()
+    for city in cities:
+        print(city)
+
+    cur.close()
+
+
+if __name__ == '__main__':
+    cities_by_state()
