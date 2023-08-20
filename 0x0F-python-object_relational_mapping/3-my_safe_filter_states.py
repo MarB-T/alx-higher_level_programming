5da59032707a26c6b3fc1ec87ae37a69cedd7f2b
MarB-T <danmarbet@gmail.com>
2023-08-20 18:13:33 +0300

    Script that gets the state given by user as an argument safely

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script to use user input argument to filter results 
+safely against sql injection
+"""
+import MySQLdb
+from sys import argv
+
+
+def get_state():
+    """ Function to get state given by user """
+    conn = MySQLdb.connect(
+                    host="localhost",
+                    port=3306,
+                    user=argv[1],
+                    passwd=argv[2],
+                    db=argv[3])
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+    search = (argv[4],)
+    cur.execute(query, search)
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    conn.close()
+
+
+if __name__ == '__main__':
+    get_state()
