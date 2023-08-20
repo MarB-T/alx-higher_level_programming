de49c55dd118d187a7f3ed2522663bec504133b0
MarB-T <danmarbet@gmail.com>
2023-08-20 05:30:32 +0300

    script that filters the results from database correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ def states_N():
     pword = sys.argv[2]
     dbname = sys.argv[3]
     conn = MySQLdb.connect(host="localhost", user=uname,
-                           passwd=pword, db=dbname, port='3306')
+                           passwd=pword, db=dbname, port=3306)
     cur = conn.cursor()
     query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
     cur.execute(query)
