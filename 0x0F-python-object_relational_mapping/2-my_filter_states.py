bbf1506d5feb2d675e56833a80eae3e079f67ccc
MarB-T <danmarbet@gmail.com>
2023-08-20 05:48:22 +0300

    Script that gets the state given by user as an argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Script to use user input argument to filter results """
+import MySQLdb
+from sys import argv
+
+
+def get_state():
+    """ Function to get state given by user """
+    conn = MySQLdb.connect(
+                    host="localhost",
+                    port=3306,
+                    user=argv[1],
+                    passwd=argv[2],
+                    db=argv[3])
+    cur = conn.cursor()
+    query = ("SELECT * FROM states WHERE name = {}".format(argv[4]))
+    cur.execute(query)
+    state = cur.fetchall()
+    print(state)
+    conn.close
+
+
+if __name__ == '__main__':
+    get_state()
