5833a3e71c3dd9b7596cea83e2fec7496355bf93
MarB-T <danmarbet@gmail.com>
2023-08-21 05:51:33 +0300

    more filters

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" lists all State objects from the database hbtn_0e_6_usa"""
+
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def search_user_input:
+    """ function to search input given bby user"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State.id).filter(State.name == sys.argv[4]).first()
+    if query is None:
+        print('Not found')
+    else:
+        print(query[0])
+
+
+if __name__ == '__main__':
+    search_user_input()
---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" lists all State objects from the database hbtn_0e_6_usa"""
+
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_city import City
+
+def fetch_by_state():
+    """function to fetch cities by state"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(City, State)\
+                   .filter(City.state_id == State.id)\
+                   .order_by(City.id)
+    for city, state in query:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+
+if __name__ == '__main__':
+    fetch_by_state()
