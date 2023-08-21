f908b534514e2469fec863f1711e41d436a43dd0
MarB-T <danmarbet@gmail.com>
2023-08-21 04:22:43 +0300

    script to create table states using sqlalchemy correction

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,7 +3,6 @@
 first python script using sqlalchemy
 """
 from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -12,10 +11,6 @@ Base = declarative_base()
 class States(Base):
     """ defines a table states """
     __tablename__ = 'states'
+
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-engine = create_engine('mysql://root:root@localhost:3306')
-
-Base.metadata.create_all(engine)
