bff59e683209fb7fec3f2f140a6c06923b379df5
MarB-T <danmarbet@gmail.com>
2023-08-21 04:29:46 +0300

    script to create table states using sqlalchemy correction2

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,7 @@
 """
 first python script using sqlalchemy
 """
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
