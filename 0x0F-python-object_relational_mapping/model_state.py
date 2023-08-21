c16a9f6e664d75c0a319b9a29ee0161d1c9896b1
MarB-T <danmarbet@gmail.com>
2023-08-21 04:32:58 +0300

    script to create table states using sqlalchemy correction3

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """
-first python script using sqlalchemy
+Contains the class definition of a State
 """
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class States(Base):
-    """ defines a table states """
+class State(Base):
+    """Defines a class State"""
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
