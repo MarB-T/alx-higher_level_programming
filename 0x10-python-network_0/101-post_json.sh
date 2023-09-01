d74a1ef5760d5a344d7e37922b3246c4959ee40c
MarB-T <danmarbet@gmail.com>
2023-09-01 05:44:02 +0300

    Update 6-peak.py and scripts to return status and post json

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# POST JSON
+curl -s -X POST -H 'Content-Type: application/json' -d @"$2" "$1"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,10 +4,9 @@ def find_peak(list_of_integers):
     """Find a Peak in a list """
     listt = list_of_integers
     l = len(listt)
-    peaks = []
     if (l <= 2):
         return (None)
     for i in range(1, l - 1):
 	    if (listt[i - 1] <= listt[i] >= listt[i + 1]):
-	        peaks.append(listt[i])
-    return (peaks)
+	        return (listt[i])
+    return (None)
