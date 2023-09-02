67844994a73ee6e43951530f8c46d1bcbf053053
MarB-T <danmarbet@gmail.com>
2023-09-02 05:13:08 +0300

    Script to send request and print error if not successful otherwise dispaly body

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL,
+sends a request to the URL
+and displays the body of the response (decoded in utf-8).
+"""
+
+import urllib.request
+import sys
+
+
+def requestt():
+    """ function to send a request"""
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode(body))
+    except urllib.error.HTTPError as error:
+        print("Error code: {}".format(error.code))
+
+
+if __name__ == '__main__':
+    requestt()
